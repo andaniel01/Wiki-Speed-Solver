1edad3f4f3877c0a47825dc9ec20541c2a057602
Andy Kim <andykim1316@gmail.com>
2021-02-16 14:22:09 -0500

    Filters through "faulty" link
    Next Step: put it in to dic

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import requests
 
 
 def get_url(url):
-    # dont know
     reqs = requests.get(url)
     soup = BeautifulSoup(reqs.text, 'html.parser')
     # storing all the urls
     urls = []
+    urls_string = []
+    urls_clean = []
     # finds all the divs with a certain id
     data = soup.find_all('div', attrs={'id': 'bodyContent'})
 
@@ -15,11 +16,30 @@ def get_url(url):
     for link in data:
         urls = link.find_all('a')
 
-
-    # making it in to click-able url
+    # changing to click-able links
+    # also adds the string version to the urls_string
     for url in urls:
-        url = "https://en.wikipedia.org/" + str(url.get('href'))
-        print(url)
+        url = "https://en.wikipedia.org" + str(url.get('href'))
+        urls_string.append(url)
+
+    # identifies the faulty urls
+    for n in range(len(urls_string)):
+        if ("/wiki/" not in urls_string[n]) or ("wiktionary.org" in urls_string[n]) or \
+                (".jpg" in urls_string[n]) or (".png" in urls_string[n]) or ("BookSources" in urls_string[n]) or \
+                (".svg" in urls_string[n]) or ("ISBN" in urls_string[n]) or ("(" in urls_string[n]) or \
+                ("Category" in urls_string[n]) or ("wikidata.org" in urls_string[n]) or ("Verifiability" in urls_string[n]) or \
+                ("Portal" in urls_string[n]) or ("Citation" in urls_string[n]) or ("wikisource" in urls_string[n]) or \
+                ("File" in urls_string[n]):
+            urls_string[n] = "temp"
+
+    # gets rid of all faulty urls
+    for n in range(len(urls_string)-1, 0, -1):
+        if urls_string[n] == "temp":
+            urls_string.remove("temp")
+
+    # testing purposes
+    for n in range(len(urls_string)):
+        print(str(n) + " " + urls_string[n])
 
 
 def main():
